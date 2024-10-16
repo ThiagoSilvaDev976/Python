7f3481a89fffbb5ee4a0bc03a5aefbe486c86157
ThiagoSilvaDev976 <thiago.programador.976@gmail.com>
2024-10-16 16:19:29 -0300

    main

--- Aula02.py
+++ Aula02.py
@@ -9,25 +9,25 @@ g = range(6)	                                #tipo range
 h = {"name" : "John", "age" : 36}	            #tipo dict	
 i = {"apple", "banana", "cherry"}	            #tipo set	
 j = frozenset({"apple", "banana", "cherry"})	#tipo frozenset	
-k = True                                        #tipobool	
+k = True                                        #tipo bool	
 l = b"Hello"	                                #tipo bytes	
 m = bytearray(5)	                            #tipo bytearray	
 n = memoryview(bytes(5))	                    #tipo memoryview	
 o = None	                                    #tipo NoneType
 
 #Tipo str (strings)
-x = str("s1") # x will be 's1'
-y = str(2)    # y will be '2'
-z = str(3.0)  # z will be '3.0'
+x = str("s1") # x será 's1'
+y = str(2)    # y será '2'
+z = str(3.0)  # z será '3.0'
 
 #Tipo int (integrais):
-x = int(1)   # x will be 1
-y = int(2.8) # y will be 2
-z = int("3") # z will be 3
+x = int(1)   # x será 1
+y = int(2.8) # y será 2
+z = int("3") # z será 3
 
 #Tipo float:
-x = float(1)     # x will be 1.0
-y = float(2.8)   # y will be 2.8
-z = float("3")   # z will be 3.0
-w = float("4.2") # w will be 4.2
+x = float(1)     # x será 1.0
+y = float(2.8)   # y será 2.8
+z = float("3")   # z será 3.0
+w = float("4.2") # w será 4.2
 
--- Aula03.py
+++ Aula03.py
@@ -1,12 +1,13 @@
+#Manipulando Strings 
 #Strings de uma linha:
 a = "eu sou uma string"
 print(a)
 
 #Strings de várias linhas:
-b = """Usando três aspas duplas seguidas,
+b = """Usando três aspas seguidas,
 podemos definir strings de mais de uma linha.
 
-mesmo pulando uma linha, o resultado se manterá correto, com o uso de três aspas duplas seguidas"""
+mesmo pulando uma linha, o resultado se manterá correto, com o uso de três aspas seguidas"""
 print(b)
 
 #Strings como Matrizes[arrays]:
@@ -14,5 +15,36 @@ c = "Cyberpunk"
 print(a[1]) #o resultado será "y", por que no python o 1º número é 0(zero)
 
 #Soletrando Strings:
-for x in "Cyberpunk":
-    print(x) #o resultado será uma letra por linha.
+for d in "Cyberpunk":
+    print(d) #o resultado será uma letra por linha.
+
+#Contandos quantas letras tem um String:
+e = "Hello, World!"
+print(len(a)) #o resultado será 13, pois conta o espaço, vírgula e exclamação.
+
+#Podemos verificar se há uma determinada palavra em uma variável:
+Crisálida = "Estágio de pupa de insetos da ordem lepidoptera, como borboletas e mariposas. A palavra vem do grego chrysallis, que significa 'dourado'. Durante a fase de crisálida, os insetos crescem e se diferenciam sexualmente."
+print("borboletas" in Crisálida) #resultado será: True, pois a palavra borboletas está na variável Crisálida
+
+#Podemos usar a condicional "if" para a mesma função acima.
+Crisálida = "Estágio de pupa de insetos da ordem lepidoptera, como borboletas e mariposas. A palavra vem do grego chrysallis, que significa 'dourado'. Durante a fase de crisálida, os insetos crescem e se diferenciam sexualmente."
+if "borboletas" in Crisálida:
+    print("Sim, 'borboletas' está no texto") #resultará na frase dentro do print.
+#Também existe a condição "not in" para verificar a negativa da afirmação, nos dois exemplos.
+
+#Fatiando Strings.
+
+#No meio do texto:
+f = "abcdef"
+print(f[2:5]) #resultará em 'cde', o 2º,3º,4º entram mas o 5º não entra. lembre que a primeira letra tem valor 0.
+
+#Do início até a posição escolhida:
+f = "abcdef"
+print(f[:3]) #resultará em 'abc'
+
+#Do ponto escolhido até o final:
+f = "abcdef"
+print(f[3:]) #resultará em 'def'
+
+#Valores negativos "coisa de corno, mas vai que..."
+f
