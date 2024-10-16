1c6da7a146dc78fc6e80cc1bf7fead4f32669f40
ThiagoSilvaDev976 <thiago.programador.976@gmail.com>
2024-10-16 16:55:55 -0300

    main

--- listabancaria.py
+++ listabancaria.py
@@ -1,5 +1,25 @@
-nome = input('Digite o Nome')
-cpf =  input('Digite seu CPF')
+nome = input('Digite o Nome: ')
+cpf =  input('Digite seu CPF: ')
 
 cliente = [nome, cpf]
 clientes = [cliente]
+
+print(f'Cadastrado o nome: {nome} e CPF: {cpf}')
+
+nome = input('Digite o nome: ')
+cpf = input('Digite o CPF: ')
+
+print(f'Cadastrado o nome: {nome} e CPF: {cpf}')
+
+cliente = [nome, cpf]
+clientes.append(cliente)
+
+print('-----------------')
+
+print("Clientes Cadastrados")
+print(f"Nome: {clientes[0][0]}")
+print(f"Cpf: {clientes[0][1]}")
+print(f"Nome: {clientes[1][0]}")
+print(f"Cpf: {clientes[1][1]}")
+
+print('-----------------')
