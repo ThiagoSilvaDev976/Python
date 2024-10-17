71090375dfa198c398257807f82f79439c9055c5
ThiagoSilvaDev976 <thiago.programador.976@gmail.com>
2024-10-17 16:37:18 -0300

    main

--- Operadores/Repeticao.py
+++ Operadores/Repeticao.py
@@ -10,6 +10,14 @@ for qualquercoisa in lista:
 else:
     print('a condição else foi aplicada no final')
 
+#usando duas variaveis (chave, valor)
 boletim = {'por':9, 'mat':8, 'hst':9, 'geo':8}
 for chave, valor in boletim.items():
     print(f'{chave}:{valor}')
+
+#for também pode ser usado para separar letras de uma string
+for nomequeeuescolher in 'Python':
+    print(nomequeeuescolher)
+
+
+#While
